10a6730bc48f0e9711b845883d08b71f5dcfb0f2
TOBY A GODFREY <toby@tobys-mbp.attlocal.net>
2017-05-17 20:24:00 -0500

    Initial Commit

---
+++ app.config.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular.module('app').config(config)
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    // this line is optional
+    //$locationProvider.html5Mode(true)
+
+
+    $stateProvider
+      .state({
+        name: 'home',
+        url: '/',
+        component: 'app',
+      })
+  }
+
+}());
---
+++ app/public/javascripts/components/pageOne.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  angular.module('myApp')
+    .component('pageOne', {
+      controller: controller,
+      template: `
+
+      <p>
+        Here is the template
+      </p>
+
+        <div class="buttons">
+          <button type="submit" class="btn btn-primary" ng-click="$ctrl.createPost($ctrl.post)">
+              Create Post
+            </button>
+        </div>
+  `
+    })
+
+
+
+  controller.$inject = ['$http']
+
+  function controller($http) {
+
+  } //end controller
+
+}());
